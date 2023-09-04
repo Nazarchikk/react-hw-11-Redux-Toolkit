c042798e6cf0bbc7efa9173def98e3a6739f387f
Ваше Ім'я <karffffagen@gmail.com>
2023-09-04 08:41:03 +0300

    fixed code

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,19 +1,32 @@
 import { nanoid } from 'nanoid'
 import { useSelector } from "react-redux";
 import { useDispatch } from 'react-redux';
-import { getFilteredContacts } from "../../redux/selectors";
+import { getContacts } from "../../redux/selectors";
 import { deleteContact } from "../../redux/operations"
 
 export default function ContactList(){
-  const contacts = useSelector(getFilteredContacts);
+  const contacts = useSelector(getContacts);
   const dispatch = useDispatch();
     return(
         <>
         <ul>
           {contacts.map(contact => (
-            <li id={nanoid(5)} key={contact.id}><span>{contact.name}</span>:<span>{contact.number}</span> <button id={"id-"+nanoid(1)} type="button" onClick={() => dispatch(deleteContact(contact.id))}>Delete</button></li>
+            <li id={nanoid(5)} key={contact.id}><span>{contact.name.name}</span>:<span>{contact.name.number}</span> <button id={"id-"+nanoid(1)} type="button" onClick={() => dispatch(deleteContact(contact.id))}>Delete</button></li>
           ))} 
         </ul>
         </>
     )
 }
+// export default function ContactList(){
+//   const contacts = useSelector(getFilteredContacts);
+//   const dispatch = useDispatch();
+//     return(
+//         <>
+//         <ul>
+//           {contacts.map(contact => (
+//             <li id={nanoid(5)} key={contact.id}><span>{contact.name}</span>:<span>{contact.number}</span> <button id={"id-"+nanoid(1)} type="button" onClick={() => dispatch(deleteContact(contact.id))}>Delete</button></li>
+//           ))} 
+//         </ul>
+//         </>
+//     )
+// }
